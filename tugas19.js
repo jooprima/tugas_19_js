d7c31e23ac0d07f3c78ce797e50c2aa854834b40
yohanes_prima <jooprima@gmail.com>
2019-07-14 23:20:26 +0700

    Tugas 19 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+console.log("semangat...".repeat(5));
+
+console.log("====================================");
+
+console.log("*".repeat(5));
+console.log("*".repeat(25));
+console.log("*".repeat(50));
+console.log("*".repeat(75));
+console.log("*".repeat(100));
+console.log("*".repeat(75));
+console.log("*".repeat(50));
+console.log("*".repeat(25));
+console.log("*".repeat(5));
